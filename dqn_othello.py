1dc87f4464cd42a17261a46b8d7c8a79cc593502
Mikołaj Michalski <>
2020-09-08 11:35:48 +0200

    dqn_othello refactored, ddqn_othello with naming change

--- dqn_othello.py
+++ dqn_othello.py
@@ -128,32 +128,90 @@ if __name__ == "__main__":
     random_iterations_to_play = 900
     games_won = 0
     max_reward = 0
-    #for e in range(EPISODES):
-    state = env.reset()     ### RESET THE ENVIRONMENT ON START OF THE GAME
-    if load_weights:  ### LOAD WEIGHTS IF PLAYING ON TRAINED MODEL
-        agent.epsilon = 0.0 ### EPSILON = 0 TO ALWAYS USE ACTIONS BASED ON MODEL CHOOSE
-        agent.load("C:\\\\Users\\mikolajm\\PycharmProjects\\pythonProject\\save\\othello-dqn-target.h5")
+    weights_to_load = "save/othello-dqn-target-new-minmax-after-refactor.h5"
+
+    def play_n_episodes(state, episodes_number, if_random, load_weights, iter_no):
+
+        full_episodes_counter = 0.
+        games_won = 0
+        if load_weights:
+            if_random = False
+            do_replay = False
+            agent.load(weights_to_load)
+            agent.epsilon = 0.0
+
         while True:
             iter_no += 1
             state = np.reshape(state, [1, state_size])
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
             next_state = np.reshape(next_state, [1, state_size])
+            if if_random:
+                agent.memorize(state, action, reward, next_state, done)
             state = next_state
             if done:
-                state = env.reset() ### RESET THE STATE IF EPISODE FINISHED
+                state = env.reset()
                 full_episodes_counter += 1
                 if reward > 0:
-                    games_won +=1
-                    print("Games won/total: {}/{}, win %: {:.4}%, last score: {}".format(games_won, full_episodes_counter, games_won/full_episodes_counter*100, reward))
+                    games_won += 1
+                    print(
+                        "{}ames won/total: {}/{}, win %: {:.4}%, last score: {}".format("Random g" if if_random else "G", games_won, full_episodes_counter,
+                                                                                       games_won / full_episodes_counter * 100,
+                                                                                       reward))
+                    writeStdOutputToFile(outputFilePath, "{}ames won/total: {}/{}, win %: {:.4}%, last score: {}".format("Random g" if if_random else "G", games_won, full_episodes_counter,
+                                                                                       games_won / full_episodes_counter * 100,
+                                                                                       reward))
                 else:
-                    print("Games won/total: {}/{}, win %: {:.4}%, last score: {}".format(games_won, full_episodes_counter,
-                                                                         games_won / full_episodes_counter * 100, reward))
-                if full_episodes_counter == EPISODES:
+                    print(
+                        "{}ames won/total: {}/{}, win %: {:.4}%, last score: {}".format("Random g" if if_random else "G",
+                                                                                          games_won,
+                                                                                          full_episodes_counter,
+                                                                                          games_won / full_episodes_counter * 100,
+                                                                                          reward))
+                    writeStdOutputToFile(outputFilePath,
+                                         "{}ames won/total: {}/{}, win %: {:.4}%, last score: {}".format(
+                                             "Random g" if if_random else "G", games_won, full_episodes_counter,
+                                             games_won / full_episodes_counter * 100,
+                                             reward))
+                if full_episodes_counter == episodes_number:
+                    if load_weights:
+                        return games_won / full_episodes_counter * 100
                     break
 
+
+
+
+    #for e in range(EPISODES):
+    state = env.reset()     ### RESET THE ENVIRONMENT ON START OF THE GAME
+    if load_weights:  ### LOAD WEIGHTS IF PLAYING ON TRAINED MODEL
+        play_n_episodes(state, EPISODES, False, load_weights, iter_no)
+        # agent.epsilon = 0.0 ### EPSILON = 0 TO ALWAYS USE ACTIONS BASED ON MODEL CHOOSE
+        # agent.load(weights_to_load)
+        # while True:
+        #     iter_no += 1
+        #     state = np.reshape(state, [1, state_size])
+        #     action = agent.act(state)
+        #     next_state, reward, done, _ = env.step(action)
+        #     next_state = np.reshape(next_state, [1, state_size])
+        #
+        #     state = next_state
+        #     if done:
+        #         state = env.reset() ### RESET THE STATE IF EPISODE FINISHED
+        #         full_episodes_counter += 1
+        #         if reward > 0:
+        #             games_won +=1
+        #             print("games won/total: {}/{}, win %: {:.4}%, last score: {}".format(games_won, full_episodes_counter, games_won/full_episodes_counter*100, reward))
+        #         else:
+        #             print("games won/total: {}/{}, win %: {:.4}%, last score: {}".format(games_won, full_episodes_counter,
+        #                                                                  games_won / full_episodes_counter * 100, reward))
+        #         if full_episodes_counter == EPISODES:
+        #             break
+
     else:  ### IF WEIGHTS ARE NOT LOADED - TRAIN THE MODEL
+        play_n_episodes(state, 30, True, load_weights, iter_no)
+
         while True:
+
             ## RANDOM PLAY UNTIL NUMBER OF ELEMENTS IN MEMORY IS EQUAL MINIBATCH SIZE
             iter_no += 1
             state = np.reshape(state, [1, state_size])
@@ -166,53 +224,85 @@ if __name__ == "__main__":
             next_state = np.reshape(next_state, [1, state_size])
             agent.memorize(state, action, reward, next_state, done)  ### SAVE STATE, ACTION, REWARD, NEXT_STATE AND DONE IN REPLY BUFFER
             state = next_state #### SET STATE TO NEXT STATE
-            random_episodes_total_reward = float(reward)
-            if done:   ### RESET STATE IF EPISODE FINISHED
-                state = env.reset()
-            if len(agent.memory) > random_iterations_to_play: #batch_size:  ### PLAY SPECIFIED NUMER OF EPISODES WITH RANDOM POLICY.
+            # random_episodes_total_reward = float(reward)
+            # if done:   ### RESET STATE IF EPISODE FINISHED
+            #     state = env.reset()
+            if len(agent.memory) > batch_size: #batch_size:  ### PLAY SPECIFIED NUMER OF EPISODES WITH RANDOM POLICY.
                 agent.replay(batch_size)
-                if done:  ### IF DONE ADD THE FINAL REWARD OF THE EPISODE TO TOTAL_REWARD AND INCREMENT FULL_EPISODES_COUNTER
+            else:
+                play_n_episodes(1, True, load_weights, iter_no)
 
-                    full_episodes_counter += 1
-                    last_n_episodes_scores.append(reward) #total_reward += reward
-                    if full_episodes_counter >= 10:
-                        #mean_reward = float(sum(last_n_episodes_scores)/len(last_n_episodes_scores)) #mean_reward = total_reward/full_episodes_counter ### CALCULATE
-                        mean_reward = sum(1 for i in last_n_episodes_scores if i == 1)/len(last_n_episodes_scores)
-                        if mean_reward == 1.0:
-                            n_episodes_deque_size += 1
-                    #if reward > max_reward:
-                    if reward > 0:
-                        agent.update_target_model()
-                        #max_reward = reward
-                        agent.save("C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-new-minmax-2.h5")
-                    print("Trained episode: {}, current score: {} mean_score: {:.4}, e: {:.2}, iterations (steps): {}".format(full_episodes_counter, reward, mean_reward, agent.epsilon, iter_no)) ### PRINT FINAL INFO ABOUT EPISODES
-                    writeStdOutputToFile(outputFilePath,"Trained episode: {}, current score: {} mean_score: {:.4}, e: {:.2}, iterations (steps): {}".format(full_episodes_counter, reward, mean_reward, agent.epsilon, iter_no))  ### PRINT FINAL INFO ABOUT EPISODES
-                    if max_mean_reward != 1.0:
-                        if mean_reward > max_mean_reward:  ### IF CURRENT MEAN_REWARD IS THE HIGHEST THEN SAVE THE WEIGHTS
-                            max_mean_reward = mean_reward
-                            print("New max mean reward!")
-                            writeStdOutputToFile(outputFilePath, "New max mean reward")
-                            agent.target_model.save(
-                                "C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-target-new-minmax-2.h5")
-                    else:
-                        if mean_reward == 1.0:
-                            print("New max mean reward!")
-                            writeStdOutputToFile(outputFilePath, "New max mean reward!")
-                            agent.target_model.save(
-                                "C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-target-new-minmax-2.h5")
 
-                if mean_reward == 1 and n_episodes_deque_size == 20:
-                    print("Solved! Iterations played: {}, full episodes played: {}".format(iter_no, full_episodes_counter))
-                    writeStdOutputToFile(outputFilePath, "Solved! Iterations played: {}, full episodes played: {}".format(iter_no, full_episodes_counter))
-                    agent.target_model.save("C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-target-new-minmax-2.h5")
-                    break
-            elif done:
-                random_episode_no += 1
-                #random_episodes_total_reward += reward
-                #random_episodes_mean_reward = random_episodes_total_reward/random_episode_no
-                print("Random episodes played: {}, random episode score: {:.4} iterations (steps): {}.".format(
-                    random_episode_no, random_episodes_total_reward, iter_no))
-                writeStdOutputToFile(outputFilePath,
-                                     "Random episodes played: {}, random episode score: {:.4} iterations (steps): {}.".format(
-                                         random_episode_no, random_episodes_total_reward, iter_no))
-                random_episodes_total_reward = 0.
+            if done:  ### IF DONE ADD THE FINAL REWARD OF THE EPISODE TO TOTAL_REWARD AND INCREMENT FULL_EPISODES_COUNTER
+                state = env.reset()
+                if reward > 0:
+                    agent.update_target_model()
+                    #max_reward = reward
+                    agent.save("C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-new-minmax-2.h5")
+                print("Trained episode: {}, current score: {} mean_score: {:.4}, e: {:.2}, iterations (steps): {}".format(full_episodes_counter, reward, mean_reward, agent.epsilon, iter_no)) ### PRINT FINAL INFO ABOUT EPISODES
+                writeStdOutputToFile(outputFilePath,"Trained episode: {}, current score: {} mean_score: {:.4}, e: {:.2}, iterations (steps): {}".format(full_episodes_counter, reward, mean_reward, agent.epsilon, iter_no))  ### PRINT FINAL INFO ABOUT EPISODES
+                full_episodes_counter += 1
+                last_n_episodes_scores.append(reward) #total_reward += reward
+                if full_episodes_counter >= 10:
+                    #mean_reward = float(sum(last_n_episodes_scores)/len(last_n_episodes_scores)) #mean_reward = total_reward/full_episodes_counter ### CALCULATE
+                    mean_reward = sum(1 for i in last_n_episodes_scores if i == 1)/len(last_n_episodes_scores)
+                    if mean_reward == 1.0:
+                        agent.target_model.save(weights_to_load)
+                        test_score = play_n_episodes(state, 50, False, True, 0)
+                        if test_score == 1.0:
+                            print("Solved! Iterations played: {}, full episodes played: {}".format(iter_no,
+                                                                                                   full_episodes_counter))
+                            writeStdOutputToFile(outputFilePath,
+                                                 "Solved! Iterations played: {}, full episodes played: {}".format(
+                                                     iter_no, full_episodes_counter))
+                            break
+                    if mean_reward > max_mean_reward:
+                        max_mean_reward = mean_reward
+                        agent.target_model.save(weights_to_load)
+                        test_score = play_n_episodes(state, 50, False, True, 0)
+                        if test_score == 1.0:
+                            print("Solved! Iterations played: {}, full episodes played: {}".format(iter_no,
+                                                                                                   full_episodes_counter))
+                            writeStdOutputToFile(outputFilePath,
+                                                 "Solved! Iterations played: {}, full episodes played: {}".format(
+                                                     iter_no, full_episodes_counter))
+                            break
+                        #n_episodes_deque_size += 1
+    #if reward > max_reward:
+            #     if reward > 0:
+            #         agent.update_target_model()
+            #         #max_reward = reward
+            #         agent.save("C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-new-minmax-2.h5")
+            #     print("Trained episode: {}, current score: {} mean_score: {:.4}, e: {:.2}, iterations (steps): {}".format(full_episodes_counter, reward, mean_reward, agent.epsilon, iter_no)) ### PRINT FINAL INFO ABOUT EPISODES
+            #     writeStdOutputToFile(outputFilePath,"Trained episode: {}, current score: {} mean_score: {:.4}, e: {:.2}, iterations (steps): {}".format(full_episodes_counter, reward, mean_reward, agent.epsilon, iter_no))  ### PRINT FINAL INFO ABOUT EPISODES
+            #     if max_mean_reward != 1.0:
+            #         if mean_reward > max_mean_reward:  ### IF CURRENT MEAN_REWARD IS THE HIGHEST THEN SAVE THE WEIGHTS
+            #             max_mean_reward = mean_reward
+            #             print("New max mean reward!")
+            #             writeStdOutputToFile(outputFilePath, "New max mean reward")
+            #             agent.target_model.save(
+            #                 "C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-target-new-minmax-2.h5")
+            #     else:
+            #         if mean_reward == 1.0:
+            #             print("New max mean reward!")
+            #             writeStdOutputToFile(outputFilePath, "New max mean reward!")
+            #             agent.target_model.save(
+            #                 "C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-target-new-minmax-2.h5")
+            #
+            # if mean_reward == 1 and n_episodes_deque_size == 20:
+            #     print("Solved! Iterations played: {}, full episodes played: {}".format(iter_no, full_episodes_counter))
+            #     writeStdOutputToFile(outputFilePath, "Solved! Iterations played: {}, full episodes played: {}".format(iter_no, full_episodes_counter))
+            #     agent.target_model.save("C:\\\\Users\\mikolajm\\OneDrive - Intel Corporation\\Desktop\\MGR\\pythonProject\\save\\othello-dqn-target-new-minmax-2.h5")
+            #     break
+            # # elif done:
+            #     random_episode_no += 1
+            #     #random_episodes_total_reward += reward
+            #     #random_episodes_mean_reward = random_episodes_total_reward/random_episode_no
+            #     print("Random episodes played: {}, random episode score: {:.4} iterations (steps): {}.".format(
+            #         random_episode_no, random_episodes_total_reward, iter_no))
+            #     writeStdOutputToFile(outputFilePath,
+            #                          "Random episodes played: {}, random episode score: {:.4} iterations (steps): {}.".format(
+            #                              random_episode_no, random_episodes_total_reward, iter_no))
+            #     random_episodes_total_reward = 0.
+
+
